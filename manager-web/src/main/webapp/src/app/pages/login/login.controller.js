e9d63847e7f1f6c140d9903000f855f3273e4d49
songying2009 <songyingjian2009@163.com>
2017-08-21 22:30:22 +0800

    增加登录时账号为空的判断，并修改登录失败的提手

--- manager-web/src/main/webapp/src/app/pages/login/login.controller.js
+++ manager-web/src/main/webapp/src/app/pages/login/login.controller.js
@@ -3,15 +3,22 @@
 
     angular.module('chicken.pages.login')
         .controller('LoginCtrl', function ($rootScope, $scope, $state, toastr, LoginService) {
-            console.log("test");
             $scope.login = function () {
+                if(!$scope.user || !$scope.user.username){
+                    toastr.error("请输入账号!");
+                    return;
+                }
                 LoginService.login($scope.user.username, $scope.user.password).then(function(data){
                     if (data && data.status === 200) {
-                        $rootScope.user = {
-                            username: $scope.user.username,
-                            password: $scope.user.password
-                        };
-                        $state.go('root.home');
+                        if(data.data.code == 0){
+                            $rootScope.user = {
+                                username: $scope.user.username,
+                                password: $scope.user.password
+                            };
+                            $state.go('root.home');
+                        }else{
+                            toastr.error(data.data.msg);
+                        }
                     }
                 }).catch(function(err) {
                     toastr.error(err);
